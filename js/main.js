c87027115da6c3f50a85e811aee89ce62cc9f376
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2020-11-25 19:39:02 -0300

    Equalizador listo para codear

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict"
 import { EquComponent } from '../components/app-equ/equ.js';
+import { SliderComponent } from '../components/app-slider/slider.js';
 import { CustomTest } from '../components/custom-test/test.js'
 import { HeaderComponent } from '../components/spoiler-header/header.js'
 
@@ -8,4 +9,5 @@ document.addEventListener("DOMContentLoaded", async function () {
     window.customElements.define('custom-test', CustomTest);
     window.customElements.define('spoiler-header', HeaderComponent);
     window.customElements.define('app-equ', EquComponent);
+    window.customElements.define('app-slider', SliderComponent);
 })
