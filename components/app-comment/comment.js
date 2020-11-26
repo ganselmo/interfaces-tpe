37fd6b5fef96c6dcec2e161efdb5d1a1f83f662b
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-26 20:13:59 -0300

    initial implementation comments

---
+++ components/app-comment/comment.js
@@ -0,0 +1,24 @@
+import { Component } from "../../../framework/components/component.js";
+
+
+export class CommentComponent extends Component {
+
+
+    constructor() {
+        super(import.meta.url)
+    }
+
+    init() {
+        // let autor = this.shadowRoot.querySelector("#comment-autor");
+        // autor.innerHTML = this.attributes.autor.value;
+    }
+
+    changes() {
+      
+    }
+
+
+
+
+
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict"
+import { CommentComponent } from '../components/app-comment/comment.js';
 import { EquComponent } from '../components/app-equ/equ.js';
 import { PlanComponent } from '../components/app-plan/plan.js';
 import { CustomTest } from '../components/custom-test/test.js'
@@ -10,4 +11,5 @@ document.addEventListener("DOMContentLoaded", async function () {
     window.customElements.define('spoiler-header', HeaderComponent);
     window.customElements.define('app-equ', EquComponent);
     window.customElements.define('app-plan', PlanComponent);
+    window.customElements.define('app-comment', CommentComponent);
 })
