6139153e6c6df9d4a8b8356f28d3789886c60d5d
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-11-27 17:17:31 -0300

    implementacion inicial comentarios

--- components/app-comment/comment.js
+++ components/app-comment/comment.js
@@ -3,22 +3,35 @@ import { Component } from "../../../framework/components/component.js";
 
 export class CommentComponent extends Component {
 
+    comments = [];
 
     constructor() {
-        super(import.meta.url)
+        super(import.meta.url);
     }
 
     init() {
-        // let autor = this.shadowRoot.querySelector("#comment-autor");
-        // autor.innerHTML = this.attributes.autor.value;
+        let btns = this.shadowRoot.querySelectorAll(".score");
+        for (let i = 0; i < btns.length; i++) {
+            const btn = btns[i];
+            btn.addEventListener("click", score);
+            btn.setAttribute("target", i);
+        }
+
+        function score() {
+            for (let i = 0; i < btns.length; i++) {
+                const element = btns[i];
+                if(element.classList.contains("scored")) {
+                    element.classList.remove("scored");
+                }
+                if(i <= this.attributes.target.value) {
+                    element.classList.add("scored");
+                }
+            }
+        }
     }
 
     changes() {
-      
+        
     }
-
-
-
-
 
 }
--- js/main.js
+++ js/main.js
@@ -13,9 +13,6 @@ import { ModeComponent } from '../Components/app-mode/mode.js';
 import { OptionsComponent } from '../Components/app-options-mov/options-mov.js';
 import { PlayerComponent } from '../Components/app-player/player.js';
 import { SliderStateComponent } from '../Components/app-slider-state/slider-state.js';
-import { SliderComponent } from '../components/app-slider/slider.js';
-import { CustomTest } from '../components/custom-test/test.js';
-import { HeaderComponent } from '../components/spoiler-header/header.js';
 
 document.addEventListener("DOMContentLoaded", async function () {
 
