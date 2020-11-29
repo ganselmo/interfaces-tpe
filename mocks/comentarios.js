3402dc81480cadf665faa80b85283757d6b5645a
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2020-11-29 20:14:09 -0300

    Push a master

---
+++ mocks/comentarios.js
@@ -0,0 +1,19 @@
+export function comentarios() {
+
+
+    [
+        {
+            author: "Geropnimo Anselmo",
+            puntos: 5,
+            fecha: "12/12/2012",
+            comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at fringilla metus. Vestibulum elementum mattis odio ut feugiat. Quisque fermentum mauris scelerisque venenatis vehicula. Aliquam ",
+            respuestas: [
+                {
+                    author: "Geropnimo Anselmo",
+                    fecha: "12/12/2012",
+                    comentario: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at fringilla metus. Vestibulum elementum mattis odio ut feugiat. Quisque fermentum mauris scelerisque venenatis vehicula. Aliquam ",
+                }
+            ]
+        }
+    ]
+}
