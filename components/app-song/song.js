cfc1f23360aa22c34ab68a6dbea1daafebf7dcd8
Javier Lopez Ferreira <javierlopez987@gmail.com>
2020-12-01 19:20:39 -0300

    efecto 3d en descripcion de cancion

--- components/app-song/song.js
+++ components/app-song/song.js
@@ -8,31 +8,41 @@ export class SongComponent extends Component {
     }
 
     init() {
-       //#region Score Stars
-       let scoreStars = this.shadowRoot.querySelectorAll(".score");
+        //#region Score Stars
+        let scoreStars = this.shadowRoot.querySelectorAll(".score");
 
-       for (let i = 0; i < scoreStars.length; i++) {
-           const scoreStar = scoreStars[i];
-           scoreStar.addEventListener("click", score);
-           scoreStar.setAttribute("target", i);
-       }
+        for (let i = 0; i < scoreStars.length; i++) {
+            const scoreStar = scoreStars[i];
+            scoreStar.addEventListener("click", score);
+            scoreStar.setAttribute("target", i);
+        }
 
-       function score() {
-           for (let i = 0; i < scoreStars.length; i++) {
-               const e = scoreStars[i];
-               if(e.classList.contains("scored")) {
-                   e.classList.remove("scored");
-               }
-               if(i <= this.attributes.target.value) {
-                   e.classList.add("scored");
-               }
-           }
-       }
-       //#endregion
+        function score() {
+            for (let i = 0; i < scoreStars.length; i++) {
+                const e = scoreStars[i];
+                if (e.classList.contains("scored")) {
+                    e.classList.remove("scored");
+                }
+                if (i <= this.attributes.target.value) {
+                    e.classList.add("scored");
+                }
+            }
+        }
+        //#endregion
+
+        //#region 3d effect
+        let display = this.shadowRoot.querySelector(".song-container");
+
+        display.addEventListener('mousemove', function (e) {
+
+            let xAxis = (window.innerWidth / 2 - e.pageX) / -30;
+            let yAxis = (window.innerHeight / 2.5 - e.pageY) / 30;
+            display.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        });
     }
 
     changes() {
-        
+
     }
 
 }
